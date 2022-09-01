317cba18131455644cf5a6226acadd79af76d518
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-08-31 19:45:15 -0700

    Add Challenge 2

--- each.rb
+++ each.rb
@@ -18,4 +18,13 @@ current_weather(weather_options)
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+def three_letter_words(words)
+  words.each do |word|
+    if word.length == 3
+      p word
+    end
+  end
+end
+three_letter_words(words)
+
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
