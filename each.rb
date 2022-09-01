1123bd10e57a282bcce364f47eccb47e97b128f7
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-08-31 19:38:52 -0700

    Initial commit

--- each.rb
+++ each.rb
@@ -1,11 +1,19 @@
 # As you complete each challenge, RUN THE CODE to ensure it works. You can do this by:
   # a. Copy and pasting it into IRB
   # b. Running the file in your Terminal. You can do this by running `ruby <filepath>`. If you are in this directory, you'd run `ruby each.rb`.
-  
+
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+def current_weather(weathers)
+  weathers.each do |weather|
+    if weather == "sunny"
+      p "The current weather is #{weather}"
+    end
+  end
+end
+current_weather(weather_options)
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
